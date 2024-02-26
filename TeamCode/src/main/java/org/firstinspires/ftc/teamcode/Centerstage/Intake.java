bb265198807abcc4785171381576585774fac54b
Devah Schaefers <devah.schaefers@icloud.com>
2024-02-25 18:41:08 -0800

    update intake values

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Centerstage/Intake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Centerstage/Intake.java
@@ -14,8 +14,8 @@ public class Intake
     enum WristPosition
     {
         LOW,
-        MID_GOING_UP,
-        MID_GOING_DOWN,
+//        MID_GOING_UP,
+//        MID_GOING_DOWN,
         HIGH
     }
 
@@ -23,16 +23,16 @@ public class Intake
     private final Servo wrist;
 
     /** High position value for wrist servo. */
-    private final double wristLow = 0.44;
+    private final double wristLow = 0.12; //old 44
 
     /** Low position value for wrist servo. */
     private final double wristMid = 0.34;
 
     /** High position value for wrist servo. */
-    private final double wristHigh = 0.12;
+    private final double wristHigh = 0;
 
     /** Current wrist position (ACTIVE = high, INACTIVE = low). */
-    private WristPosition wristPosition = WristPosition.MID_GOING_UP;
+    private WristPosition wristPosition = WristPosition.LOW;
 
     /** Servo for claw mechanism. */
     private final Servo claw1;
@@ -76,21 +76,25 @@ public class Intake
     public void toggleWristPosition()
     {
         if (wristPosition == WristPosition.LOW) {
-            setWristMid();
-            wristPosition = WristPosition.MID_GOING_UP;
-        }
-        else if(wristPosition == WristPosition.MID_GOING_UP) {
             setWristHigh();
             wristPosition = WristPosition.HIGH;
         }
-        else if(wristPosition == WristPosition.HIGH) {
-            setWristMid();
-            wristPosition = WristPosition.MID_GOING_DOWN;
-        }
-        else if(wristPosition == WristPosition.MID_GOING_DOWN) {
+        else {
             setWristLow();
             wristPosition = WristPosition.LOW;
         }
+//        else if(wristPosition == WristPosition.MID_GOING_UP) {
+//            setWristHigh();
+//            wristPosition = WristPosition.HIGH;
+//        }
+//        else if(wristPosition == WristPosition.HIGH) {
+//            setWristMid();
+//            wristPosition = WristPosition.MID_GOING_DOWN;
+//        }
+//        else if(wristPosition == WristPosition.MID_GOING_DOWN) {
+//            setWristLow();
+//            wristPosition = WristPosition.LOW;
+//        }
     }
 
     /** Toggles claw between open and closed positions. */
@@ -118,25 +122,25 @@ public class Intake
         claw2.setPosition(clawClosePosition);
     }
 
-    /** Sets wrist to its low position. */
+//    /** Sets wrist to its low position. */
     public void setWristLow()
     {
         wrist.setPosition(wristLow);
     }
-
-    // uhh delete this later
-    public void xd() { wrist.setPosition(0.42); }
-
-    /** Sets wrist to its high position. */
+//
+//    // uhh delete this later
+//    public void xd() { wrist.setPosition(0.42); }
+//
+//    /** Sets wrist to its high position. */
     public void setWristHigh()
     {
         wrist.setPosition(wristHigh);
     }
-
-    public void setWristMid()
-    {
-        wrist.setPosition(wristMid);
-    }
+//
+//    public void setWristMid()
+//    {
+//        wrist.setPosition(wristMid);
+//    }
 
     public void updateServos(boolean clawButton, boolean wristButton)
     {
