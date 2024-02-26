91d9ddee4b4940b5cd6419bdb9cc9bce01732756
park8749 <parks8749@gmail.com>
2024-02-25 21:32:16 -0800

    finish auto scoring without driving

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/AutonomousEasyBlue2024.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/AutonomousEasyBlue2024.java
@@ -20,6 +20,6 @@ public class AutonomousEasyBlue2024 extends LinearOpMode
 //        driver.turn_90_clockwise(1);
 //        driver.forward_tiles(1);
 //        driver.turn_90_counter_clockwise(1);
-        scorer.score(driver);
+//        scorer.score(driver);
     }
 }
