a84c3654e119e46952be76cdff13df9f1046aad5
Wesley Zeng <wesleyzeng08@gmail.com>
2024-03-03 16:07:41 -0800

    added easy/hard blue/red parking

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/ParkingAutonomousHardBlue2024.java
@@ -0,0 +1,23 @@
+package org.firstinspires.ftc.teamcode.Autonomous;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+@Autonomous(name="ParkingAutonomousHardBlue2024", group="Autonomous")
+public class ParkingAutonomousHardBlue2024 extends LinearOpMode
+{
+    Driver driver;
+    Scorer scorer;
+    int tile_length = 24; //in inches
+    public void runOpMode()
+    {
+        driver = new Driver(hardwareMap);
+        scorer = new Scorer(hardwareMap);
+
+        waitForStart();
+        scorer.load();
+        driver.forward_tiles(0.1);
+        driver.turn_90_counter_clockwise(1);
+        driver.forward_tiles(4);
+    }
+}
