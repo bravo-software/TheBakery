01029b191b609045f90bc40bb43f122a1819b15a
park8749 <parks8749@gmail.com>
2024-10-26 16:40:21 -0700

    arm and turn works

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/CenterStage2023.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/CenterStage2023.java
@@ -2,41 +2,114 @@ package org.firstinspires.ftc.teamcode.TeleOp;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
+//import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
-import com.qualcomm.robotcore.util.SerialNumber;
 
+import org.firstinspires.ftc.teamcode.Centerstage.Intake;
+import org.firstinspires.ftc.teamcode.Core.DriveTrain;
+import org.firstinspires.ftc.teamcode.Core.Launcher;
+import org.firstinspires.ftc.teamcode.Core.LinearSlides;
 
-@TeleOp(name="CenterStage2023", group="TeleOp")
+@TeleOp(name="Test2024", group="TeleOp")
+public class Test2024 extends LinearOpMode {
 
-public class CenterStage2023 extends LinearOpMode
-{
-    //    private Servo wrist;
+//    public DriveTrain driveTrain;
+    private Servo scoop;
+    private Servo turn;
+    private Servo arm;
+
     @Override
     public void runOpMode()
     {
-//        wrist = hardwareMap.get(Servo.class, (SerialNumber) wrist);
-        DcMotor test = hardwareMap.get(DcMotor.class, "test");
-        test.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        test.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        telemetry.addData("test", test.getCurrentPosition());
+        scoop = hardwareMap.get(Servo.class, "scoop");
+        arm = hardwareMap.get(Servo.class, "arm");
+        turn = hardwareMap.get(Servo.class, "turn");
+
+//        driveTrain = new DriveTrain(hardwareMap, "fL", "bL", "fR", "bR");
+//        //Linear Slides
+//        int linearSlidesPosition = 1300;
+//
+//        LinearSlides linearSlides = new LinearSlides(hardwareMap, "Slides", linearSlidesPosition);
+//
+//        Intake intake = new Intake(hardwareMap, "wrist", "claw1", "claw2");
+//
+////        HookMechanism hookMechanism = new HookMechanism(hardwareMap, "hanging", "hook");
+//
+//        Launcher launcher = new Launcher(hardwareMap, "launcher");
+//
+        telemetry.addData("Status", "Initialized");
         telemetry.update();
         waitForStart();
 
         while (opModeIsActive())
         {
-//            wrist.setDirection(Servo.Direction.FORWARD);
-//            wrist.setPosition(0.3);
-            telemetry.clear();
-            telemetry.addData("test", test.getCurrentPosition());
+            arm.setDirection(Servo.Direction.FORWARD);
+            if (gamepad2.b) {
+                arm.setPosition(0.9);
+            }
+            else if (gamepad2.y) {
+                arm.setPosition(0.482);
+            }
+            telemetry.addData("Servo Position", arm.getPosition());
             telemetry.update();
+            telemetry.clear();
 
-            test.setPower(0.1);
+            turn.setDirection(Servo.Direction.FORWARD); // or REVERSE
+            // Control the continuous servo based on gamepad input
+            if (gamepad2.left_stick_y > 0) {
+                // Full speed clockwise
+                turn.setPosition(-1);
+            } else if (gamepad2.left_stick_y < 0) {
+                // Full speed counter-clockwise
+                turn.setPosition(1.0);
+            } else if (gamepad2.left_stick_x < 0) {
+                turn.setPosition(0);
+            }
+
+                // Add telemetry or other controls as needed
+                telemetry.update();
+
+
+
+            scoop.setDirection(Servo.Direction.FORWARD);
+            if (gamepad2.a) { // goes counter-clockwise.
+                scoop.setPosition(0.8);
+            }
+            else if (gamepad2.x) { // goes clockwise.
+                scoop.setPosition(1.0);
+            }
+            telemetry.addData("Servo Position", scoop.getPosition());
+            telemetry.update();
+            telemetry.clear();
+
+
+
+//            telemetry.addData("Slides Pos", String.valueOf(linearSlides.motor.getCurrentPosition()));
+//            telemetry.addData("FR Pos", String.valueOf(driveTrain.MotorFR.getCurrentPosition()));
+//            telemetry.addData("FL Pos", String.valueOf(driveTrain.MotorFL.getCurrentPosition()));
+//            telemetry.addData("BR Pos", String.valueOf(driveTrain.MotorBR.getCurrentPosition()));
+//            telemetry.addData("BL Pos", String.valueOf(driveTrain.MotorBL.getCurrentPosition()));
+//
+//            telemetry.update();
+
+
+            //Controller 1
+//            driveTrain.Drive(gamepad1);
+//            hookMechanism.update(gamepad1);
+
+
+            //Controller 2
+//            intake.updateServos(gamepad2.x, gamepad2.a);
+//            linearSlides.update(gamepad2.y);
+//            launcher.update(gamepad2.b);
 
         }
-
-
-
     }
+
 }
+
+// centerstage doesn't work on drivershub for some reason
+// so use Test2024 to run stuff and rn it has random stuff.
+// will have servo code there so I can test it.
+// then I will have chassi code to try to move the robot.
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/Test2024.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/Test2024.java
@@ -3,14 +3,20 @@ package org.firstinspires.ftc.teamcode.TeleOp;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.util.SerialNumber;
 
-@TeleOp(name="Test2024", group="TeleOp")
 
-public class Test2024 extends LinearOpMode
+@TeleOp(name="CenterStage2023", group="TeleOp")
+
+public class CenterStage2023 extends LinearOpMode
 {
+    //    private Servo wrist;
     @Override
     public void runOpMode()
     {
+//        wrist = hardwareMap.get(Servo.class, (SerialNumber) wrist);
         DcMotor test = hardwareMap.get(DcMotor.class, "test");
         test.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         test.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
@@ -20,6 +26,8 @@ public class Test2024 extends LinearOpMode
 
         while (opModeIsActive())
         {
+//            wrist.setDirection(Servo.Direction.FORWARD);
+//            wrist.setPosition(0.3);
             telemetry.clear();
             telemetry.addData("test", test.getCurrentPosition());
             telemetry.update();
