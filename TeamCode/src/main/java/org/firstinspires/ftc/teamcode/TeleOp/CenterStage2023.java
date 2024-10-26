10441c161d526056873b1eae6e1c6165f5946bf8
park8749 <parks8749@gmail.com>
2024-10-26 16:35:23 -0700

    updated Test2024 (arm and turn works and set)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/CenterStage2023.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/CenterStage2023.java
@@ -2,61 +2,41 @@ package org.firstinspires.ftc.teamcode.TeleOp;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.util.SerialNumber;
 
-import org.firstinspires.ftc.teamcode.Centerstage.Intake;
-import org.firstinspires.ftc.teamcode.Core.DriveTrain;
-import org.firstinspires.ftc.teamcode.Core.Launcher;
-import org.firstinspires.ftc.teamcode.Core.LinearSlides;
 
 @TeleOp(name="CenterStage2023", group="TeleOp")
-public class CenterStage2023 extends LinearOpMode {
 
-    public DriveTrain driveTrain;
-
+public class CenterStage2023 extends LinearOpMode
+{
+    //    private Servo wrist;
     @Override
     public void runOpMode()
     {
-
-        driveTrain = new DriveTrain(hardwareMap, "fL", "bL", "fR", "bR");
-        //Linear Slides
-        int linearSlidesPosition = 1300;
-
-        LinearSlides linearSlides = new LinearSlides(hardwareMap, "Slides", linearSlidesPosition);
-
-        Intake intake = new Intake(hardwareMap, "wrist", "claw1", "claw2");
-
-//        HookMechanism hookMechanism = new HookMechanism(hardwareMap, "hanging", "hook");
-
-        Launcher launcher = new Launcher(hardwareMap, "launcher");
-
-        telemetry.addData("Status", "Initialized");
+//        wrist = hardwareMap.get(Servo.class, (SerialNumber) wrist);
+        DcMotor test = hardwareMap.get(DcMotor.class, "test");
+        test.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        test.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        telemetry.addData("test", test.getCurrentPosition());
         telemetry.update();
         waitForStart();
 
         while (opModeIsActive())
         {
+//            wrist.setDirection(Servo.Direction.FORWARD);
+//            wrist.setPosition(0.3);
             telemetry.clear();
-            telemetry.addData("Slides Pos", String.valueOf(linearSlides.motor.getCurrentPosition()));
-            telemetry.addData("FR Pos", String.valueOf(driveTrain.MotorFR.getCurrentPosition()));
-            telemetry.addData("FL Pos", String.valueOf(driveTrain.MotorFL.getCurrentPosition()));
-            telemetry.addData("BR Pos", String.valueOf(driveTrain.MotorBR.getCurrentPosition()));
-            telemetry.addData("BL Pos", String.valueOf(driveTrain.MotorBL.getCurrentPosition()));
-
+            telemetry.addData("test", test.getCurrentPosition());
             telemetry.update();
 
-
-            //Controller 1
-            driveTrain.Drive(gamepad1);
-//            hookMechanism.update(gamepad1);
-
-
-            //Controller 2
-            intake.updateServos(gamepad2.x, gamepad2.a);
-            linearSlides.update(gamepad2.y);
-            launcher.update(gamepad2.b);
+            test.setPower(0.1);
 
         }
+
+
+
     }
-
 }
-
